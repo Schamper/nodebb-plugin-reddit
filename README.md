5e1b7bc7f9fb61d6bb628799bb97dae74c89a749
Schamper <erikschamper@outlook.com>
2014-09-10 20:03:04 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# NodeBB Reddit Plugin
+
+This NodeBB plugin will create a hyperlink to a subreddit if it's mentioned using `/r/subreddit` syntax.
+
+## Installation
+
+    npm install nodebb-plugin-reddit
+
---
+++ library.js
@@ -0,0 +1,14 @@
+(function(Reddit) {
+	"use strict";
+	var anchor = '<a href="http://reddit.com$1" target="_blank">$1</a>';
+
+	Reddit.parse = function(postContent, callback) {
+		var	regex = /(\/r\/\w+)/g;
+
+		if (postContent.match(regex)) {
+			postContent = postContent.replace(regex, anchor);
+		}
+
+		callback(null, postContent);
+	};
+}(module.exports));
