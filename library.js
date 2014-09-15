b8fb8cb98d070481c4ec88230c3f21325129ffde
MrWaffle <mrwafflewaffle@aim.com>
2014-09-15 11:19:57 +0200

    Update regex Only match if /r/* is the beginning of the string or if it is prefixed by a whitespace

--- library.js
+++ library.js
@@ -3,7 +3,7 @@
 	var anchor = '<a href="http://reddit.com$1" target="_blank">$1</a>';
 
 	Reddit.parse = function(postContent, callback) {
-		var	regex = /(\/r\/\w+)/g;
+		var	regex = /(?:^|\s)(\/r\/\w+)/g;
 
 		if (postContent.match(regex)) {
 			postContent = postContent.replace(regex, anchor);
