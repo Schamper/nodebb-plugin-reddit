e1bd4b8b9c66b0c1076d756b059abcff4cc85f00
mputters <michael.putters@binarygenetics.com>
2014-12-27 13:46:37 +0000

    Compatibility with NodeBB 0.6.x

--- library.js
+++ library.js
@@ -2,13 +2,14 @@
 	"use strict";
 	var anchor = '<a href="http://reddit.com$1" target="_blank">$1</a>';
 
-	Reddit.parse = function(postContent, callback) {
+	Reddit.parse = function(data, callback) {
 		var	regex = /\B(\/[ru]\/\w+)/g;
+		var postContent = data && data.postData && data.postData.content;
 
-		if (postContent.match(regex)) {
-			postContent = postContent.replace(regex, anchor);
+		if (postContent && postContent.match(regex)) {
+			data.postData.content = postContent.replace(regex, anchor);
 		}
 
-		callback(null, postContent);
+		callback(null, data);
 	};
 }(module.exports));
