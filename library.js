278aba7200463dbc33a74e3812bdb2805b3294b4
Schamper <mrwafflewaffle@aim.com>
2015-02-10 22:01:09 +0100

    Add support for the raw parsing hook

--- library.js
+++ library.js
@@ -1,15 +1,28 @@
 (function(Reddit) {
+
 	"use strict";
 	var anchor = '<a href="http://reddit.com$1" target="_blank">$1</a>';
 
-	Reddit.parse = function(data, callback) {
-		var	regex = /\B(\/[ru]\/\w+)/g;
+	Reddit.parsePost = function(data, callback) {
 		var postContent = data && data.postData && data.postData.content;
 
-		if (postContent && postContent.match(regex)) {
-			data.postData.content = postContent.replace(regex, anchor);
-		}
+        data.postData.content = parse(postContent);
 
 		callback(null, data);
 	};
+
+    Reddit.parseRaw = function(raw, callback) {
+        callback(null, parse(raw));
+    };
+
+    function parse(text) {
+        var	regex = /\B(\/[ru]\/\w+)/g;
+
+        if (text && text.match(regex)) {
+            return text.replace(regex, anchor);
+        } else {
+            return text;
+        }
+    }
+
 }(module.exports));
